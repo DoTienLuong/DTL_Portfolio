1737595ac362c9b81c54636476b4c05c6a901ec7
dotienluong <dotienluong1402@gmail.com>
2025-07-30 12:30:40 +0700

    Update: Hoan thanh footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+      <div className='text-center'>
+        <Image src={assets.logo} alt='logofooter' className='w-36 mx-auto mb-2' />
+        <div className='w-max flex items-center gap-2 mx-auto'>
+            <Image src={assets.mail_icon} alt='' className='w-6'/>
+            dotienluong1402@gmail.com
+        </div>
+      </div>
+      <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+        <p>2025 Developed by Do Tien Luong</p>
+        <ul className='flex items-center justify-center gap-10 mt-4 sm:mt-0'>
+            <li><a target='_blank' href="linkgithub">Github</a></li>
+            <li><a target='_blank' href="linkgithub">Linkedln</a></li>
+            <li><a target='_blank' href="linkgithub">Facebook</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
