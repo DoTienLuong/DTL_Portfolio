74dd63ef240c9be753643cc35cab50f3aea22d96
dotienluong <dotienluong1402@gmail.com>
2025-07-30 10:40:05 +0700

    Update: hoan thanh my services

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Services from "./components/Services";
 export default function Home() {
   return (
     <>
       <NavBar />
       <Header />
       <About />
+      <Services />
     </>
   );
 }
