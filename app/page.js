7b1ebdfd67b35e8d38a72658d552dadcdc08ff71
dotienluong <dotienluong1402@gmail.com>
2025-07-30 11:34:21 +0700

    Update: hoan thanh Work

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Services from "./components/Services";
+import Work from "./components/Work";
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <About />
       <Services />
+      <Work />
     </>
   );
 }
